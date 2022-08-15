fd3ed3660c645612dec30a410ba62dddc154c082
Tom Rossner <tomrossner2@gmail.com>
2022-08-15 03:44:48 +0300

    Added features and fixed bugs

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ const addMoreInput = document.querySelector("#addMore");
 const playIcon = document.querySelector("#play.gg-play-button");
 const stopIcon = document.querySelector("#stop.gg-play-stop");
 turnOff(stopIcon);
+const trashContainer = document.querySelector("#trashContainer");
 const trashBtn = document.querySelector("#trashBtn");
+const trashChevronDownIcon = document.querySelector("#trashChevronDownIcon");
+const clearOptionsContainer = document.querySelector(".clear-options");
+const clearCurrent = document.querySelector("#clearCurrent");
+const clearAll = document.querySelector("#clearAll");
 const imgContainer = document.querySelector(".img-container");
 const chevronLeft = document.querySelector(".chevronBox.left");
 const chevronRight = document.querySelector(".chevronBox.right");
@@ -32,7 +37,7 @@ let autoPlayStatus = false;
 let autoPlay = setInterval(function(){
     imgArray.push(imgArray.shift());
     for(let i = 0; i < imgArray.length; i++){
-        displayNone(imgArray[i]);
+        turnOff(imgArray[i]);
         imgArray[0].style.display = "block";
         imgArray[0].style.animation = `${delay / 1000}s scale, fadeIn 2s`;
     }
@@ -45,13 +50,10 @@ count === 0 ? turnOff(trashBtn) : turnOn(trashBtn);
 count === 0 ? turnOff(playBtn) : turnOn(playBtn);
 
 
-    
-
-
 // EVENT LISTENERS
 
 playBtn.addEventListener("click", () =>{
-    checkArrayLength(imgArray);
+    canUseAutoPlay(imgArray);
     if(imgArray.length > 1){
         handleBtnContent(playIcon, stopIcon);
         if(autoPlayStatus === false){
@@ -64,8 +66,11 @@ playBtn.addEventListener("click", () =>{
                     imgArray[0].style.display = "block";
                     imgArray[0].style.animation = `${delay / 1000}s scale, fadeIn 2s`;
                 }
-                };
+            };
             autoPlayStatus = true;
+            if(clearOptionsContainer.classList.contains("open")){
+                closeTrashOptions();
+            }
             disableButton(trashBtn);
             removePointerEvents(trashBtn);
             handleInfoMsgs(autoPlayStatus, count);
@@ -123,7 +128,7 @@ addMoreInput.addEventListener("change", () => {
         imgArray[0].style.display = "block";
         imgArray[0].classList.toggle("active");
         enableButton(trashBtn);
-        updateCount(imgArray);
+        updateCountMsg(imgArray);
         handleInfoMsgs(status, count);
         count > 1 ? enableButton(playBtn) : disableButton(playBtn);
         count > 1 ? enableButton(delayBtn) : disableButton(delayBtn);
@@ -136,7 +141,13 @@ chevronRight.addEventListener("click", () => {
         displayNone(imgArray[i]);
         imgArray[i].classList.remove("active");
     }
-    displayImg(currentIndex + 1);
+    // Update current index
+    currentIndex += 1;
+    currentIndex = determineCurrentIndex(currentIndex, 0, imgArray.length-1);
+   
+    // Display Current image
+    currentImage = imgArray[currentIndex];
+    displayImg(currentImage);
     
 })
 
@@ -146,8 +157,14 @@ chevronLeft.addEventListener("click", () => {
         displayNone(imgArray[i]);
         imgArray[i].classList.remove("active");
     }
-    displayImg(currentIndex - 1);
-})
+    // Update current index
+    currentIndex -= 1;
+    currentIndex = determineCurrentIndex(currentIndex, 0, imgArray.length-1);
+  
+    // Display Current image
+    currentImage = imgArray[currentIndex];
+    displayImg(currentImage);
+});
 
 zoomIn.addEventListener("click", () => {
     imgContainer.classList.toggle("expanded");
@@ -163,28 +180,33 @@ zoomOut.addEventListener("click", () => {
     disableButton(zoomOut);
 })
 
-trashBtn.addEventListener("click", () => {
-    updateCount(imgArray);
+clearAll.addEventListener("click", () => {
+
     disableButton(playBtn);
     disableButton(delayBtn);
     disableButton(trashBtn);
-    if(count === 0){
-        imgContainerParagraph.innerHTML = "No images to clear.";
-        imgContainerParagraph.style.display = "block";
-        imgContainerParagraph.style.color = "white";
-        imgContainerParagraph.style.backgroundColor = "#c1121f";
-        disableButton(trashBtn);
-        return;
-    }
-    clearArray(imgArray);
-    updateCount(imgArray);
+    closeTrashOptions();
+    // disableButton(trashBtn);
+    imgContainerParagraph.innerHTML = `Cleared  ${count} images.`;
+    imgContainerParagraph.style.display = "block";
+    imgContainerParagraph.style.color = "white";
+    imgContainerParagraph.style.backgroundColor = "#c1121f";
+    imgArray = clearArray(imgArray);
+    count = imgArray.length;
+
+    updateCountMsg(imgArray);
     handleInfoMsgs(status, count);
     collapseImgContainer(imgArray);
 })
 
 setDelayBtn.addEventListener("click", () => {
     delay = delayInput.value;
-    delay < 2 ? delay = 2 : delay;
+    playingMsg.style.opacity = 1;
+    playingMsg.innerHTML = `Delay set to ${delay} seconds.`;
+    if(delay < 2){
+        delay = 5;
+        playingMsg.innerHTML = `Delay invalid. Delay set to ${delay} seconds.`;
+    }
     delay *= 1000;
     turnOff(setDelayBtn);
     turnOff(delayInput);
@@ -211,6 +233,9 @@ delayBtn.addEventListener("click", () => {
         turnOn(playIcon);
         turnOff(stopIcon);
         playBtn.innerHTML = `<i id="play" class="gg-play-button"></i><span>Play</span>`;
+        enableButton(playingMsg);
+        playingMsg.innerHTML = "Set a delay in seconds -- Default delay is set to 5 seconds, minimum delay is 2 seconds.";
+        playingMsg.style.opacity = 1;
         disableButton(trashBtn);
         removePointerEvents(trashBtn);
         for(let i = 0; i < imgArray.length; i++){
@@ -218,28 +243,78 @@ delayBtn.addEventListener("click", () => {
             imgArray[i].style.animation = "";
         }
         imgArray[0].style.display = "block";
-        handleInfoMsgs(autoPlayStatus, count);
+        // handleInfoMsgs(autoPlayStatus, count);
         chevronLeft.style.display = "";
         chevronRight.style.display = "";
+        if(clearOptionsContainer.classList.contains("open")){
+            closeTrashOptions();
+        }
+    }
+})
+
+trashBtn.addEventListener("click", () => {
+    if(count === 0){
+        imgContainerParagraph.innerHTML = `No images to clear.`;
+        imgContainerParagraph.style.display = "block";
+        imgContainerParagraph.style.color = "white";
+        imgContainerParagraph.style.backgroundColor = "#c1121f";
+        turnOff(trashBtn);
+        return;
+    }
+    trashBtn.classList.remove("off");
+    clearOptionsContainer.classList.toggle("open");
+    trashChevronDownIcon.classList.toggle("open");
+})
+
+clearCurrent.addEventListener("click", () => {
+    // Delete current image from HTML
+    currentImg = imgArray[currentIndex];
+    currentImg.remove();
+    
+    // Remove current image from imgArray
+    imgArray = clearIndex(imgArray, currentIndex);
+    collapseImgContainer(imgArray);
+    closeTrashOptions();
+
+    // Update the current index
+    currentIndex += 1;
+    currentIndex = determineCurrentIndex(currentIndex, 0, imgArray.length-1);
+
+    // Messages
+    updateCountMsg(imgArray);
+    handleInfoMsgs(status, count);
+    if(count === 0){
+        imgContainerParagraph.innerHTML = "No images.";
+        imgContainerParagraph.style.display = "block";
+        imgContainerParagraph.style.color = "white";
+        imgContainerParagraph.style.backgroundColor = "#c1121f";
+        disableButton(trashBtn);
+        disableButton(playBtn);
+        disableButton(delayBtn);
+        return;
+    }
+    // Update the displayed image
+    displayImg(imgArray[currentIndex]);
+    
+    function clearIndex(array, elementIndexToClear){
+        // Clears the the element in the array at current index.
+        array.splice(elementIndexToClear, 1);
+        return array;
     }
 })
 
 
+
 // FUNCTIONS
 
-function displayImg(i){
-    if(i >= imgArray.length){i = 0;}
-    if(i < 0){i = imgArray.length - 1;}
-    
-    imgArray[currentIndex].classList.remove("active");
-    displayNone(imgArray[currentIndex]);
-    imgArray[i].style.display = "block";
-    imgArray[i].classList.add("active");
-
-    currentIndex = i;
+function displayImg(image){
+    image.style.display = "block";
+    image.classList.add("active");
 }
 
-function checkArrayLength(array){
+function canUseAutoPlay(array){
+// Checks if the array's length is smaller or equal to 1. If it is, display error message.
+// If greater than 1, removes error message if visible, and enables the play button.
     if (array.length <= 1){
         imgContainerParagraph.style.display = "block";
         imgContainerParagraph.style.color = "white";
@@ -258,6 +333,7 @@ function checkArrayLength(array){
 }
 
 function handleBtnContent(icon1, icon2){
+// Checks which icon is displayed on the play button, toggles between them.
     if(icon1.classList.contains("off")){
         turnOn(icon1);
         turnOff(icon2);
@@ -271,55 +347,82 @@ function handleBtnContent(icon1, icon2){
 }
 
 function collapseImgContainer(array){
+// Checks if the array is smaller than 1, if it is, collapses the image container, otherwise opens it.
     array.length < 1 ? displayNone(imgContainer) : imgContainer.style.display = "flex";
 }
 
-function updateCount(array){
+function updateCountMsg(array){
+// Updates count message based on the array's length.
     count = array.length;
     count <= 1 ? imgCount.innerHTML = `${count} image` : imgCount.innerHTML = `${count} images`;
 }
 
 function handleInfoMsgs(status, count){
+// Checks if the autoPlay function is on, displays playing message.
+// Sets opacity to 0 if the image count is smaller than 1, otherwise sets opacity to 1. 
     status === true ? playingMsg.style.opacity = 1 : playingMsg.style.opacity = 0;
     status === true ? playingMsg.innerHTML = `Delay set to ${delay / 1000} seconds, playing...` : playingMsg.style.opacity = 0;
     count < 1 ? imgCount.style.opacity = 0 : imgCount.style.opacity = 1;
 }
 
 function clearArray(array){
+// Checks if each array element is of type "object", removes it from the DOM if it is.
+// Sets the array's length to 0 and updates count variable.
     for(let img of array){
         if(typeof img === "object"){
             img.remove();
         }
     }
-    array.length = 0;
-    count = array.length;
+    array = [];
+    return array;
 }
 
 function clearInput(input){
+// Clears input value.
     input.value = "";
 }
 
 function disableButton(button){
+// Adds a class that disables a button.
     button.classList.add("disabled");
 }
 
 function removePointerEvents(button){
+// Removes all pointer events from a button so it cannot be clicked.
     button.style.pointerEvents = "none";
 }
 
 function enableButton(button){
+// Removes the "disabled" class and adds all pointer events back.
     button.classList.remove("disabled");
     button.style.pointerEvents = "all";
 }
 
 function turnOff(element){
+// Turns off element.
     element.classList.add("off");
 }
 
 function turnOn(element){
+// Turns on element.
     element.classList.remove("off");
 }
 
 function displayNone(element){
+// Sets display none to element.
     element.style.display = "none";
+}
+
+function determineCurrentIndex(index, smallestIndex, largestIndex){
+    if(index < 0){
+        index = largestIndex;
+    } else if(index > largestIndex){
+        index = smallestIndex;
+    }
+    return index;
+}
+
+function closeTrashOptions(){
+    trashChevronDownIcon.classList.remove("open");
+    clearOptionsContainer.classList.remove("open");
 }
